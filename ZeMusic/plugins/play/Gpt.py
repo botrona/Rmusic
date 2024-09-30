a4928fc1fed14dbb5ab4c15acc1a545ccb132e21
bblks <168915058+bblks@users.noreply.github.com>
2024-09-30 21:29:27 +0300

    Update Gpt.py

--- ZeMusic/plugins/play/Gpt.py
+++ ZeMusic/plugins/play/Gpt.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 
-@app.on_message(filters.command(["اليسا"],""))
+@app.on_message(filters.command(["اليسا"],"") & filters.group)
 async def chat_gpt(bot, message):
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
